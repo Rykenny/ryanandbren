885d8d0a087bfdf5acaabff0b79da090ffe7cfa2
Ryan <ryan@Ryans-MacBook-Air.local>
2025-05-31 16:42:41 -0400

    changing logging

--- server/server.js
+++ server/server.js
@@ -107,7 +107,7 @@ app.get('/partyData/:partyCode', async (req, res) => {
   console.log('Received party_code:', partyCode);
   const data = await fetchPartyData(partyCode);
   res.json(data);
-  console.log('party_code response:', json(data));
+  console.log('party_code response:', res.json(data));
 });
 
 app.get('/health_check', (req, res) => {
